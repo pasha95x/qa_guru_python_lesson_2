b0237862874d90310e4c2931fec83d103b02bc71
Pavel <pavel.pestrikov@simbirsoft.com>
2023-08-02 16:12:37 +0400

    fix requirements.txt and conftest.py

--- conftest.py
+++ conftest.py
@@ -3,9 +3,11 @@ from selene import browser
 from selenium import webdriver
 
 
-@pytest.fixture()
+@pytest.fixture(scope='function', autouse=True)
 def browser_size():
     driver = webdriver.Chrome()
     browser.config.driver = driver
-    # Setting the window size to 1200 * 800
-    driver.set_window_size(200, 800)
+    # Setting the window size to 1920 * 1080
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+
--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,7 +2,7 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_1(browser_size):
+def test_1():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
