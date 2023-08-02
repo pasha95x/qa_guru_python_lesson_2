535a5bab1af9dc972becb36ae0c7c53f34fcb4b4
Pavel <138729653+pasha95x@users.noreply.github.com>
2023-08-03 00:31:08 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,8 +5,6 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_size():
-    driver = webdriver.Chrome()
-    browser.config.driver = driver
     # Setting the window size to 1920 * 1080
     browser.config.window_height = 1080
     browser.config.window_width = 1920
